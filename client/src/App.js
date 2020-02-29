d9daed637901013046f3be95835d42d32517bf11
timramone <timramone@gmail.com>
2020-02-29 15:09:05 +0300

    *

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 class App extends Component {
   render() {
@@ -13,11 +13,10 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
+            <Link to="/">Fib calculator v2</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
